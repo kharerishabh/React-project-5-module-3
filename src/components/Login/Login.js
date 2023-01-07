57c74987271993c42386b7a705cbf34562f245bd
kharerishabh <rishabkhare41@gmail.com>
2023-01-07 17:20:03 +0530

    module-3 project 5 task 8

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,59 +1,115 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+const emailReducer = (state, action) => {
+  if(action.type === 'USER_INPUT'){
+    return {value: action.val, isValid: action.val.includes('@')}
+  }
+  if(action.type === 'INPUT_BLUR'){
+    return {value: state.value, isValid: state.value.includes('@')}
+  }
+  return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if(action.type === 'USER_INPUT'){
+    return {value: action.val, isValid: action.val.trim().length > 6}
+  }
+  if(action.type === 'INPUT_BLUR'){
+    return {value: state.value, isValid: state.value.trim().length > 6}
+  }
+  return { value: "", isValid: false };
+}
 
 const Login = (props) => {
-  const [enteredCollegeName, setEnteredCollegeName] = useState('')
-  const [collegeNameIsValid, setCollegeNameIsValid] = useState()
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [enteredCollegeName, setEnteredCollegeName] = useState("");
+  const [collegeNameIsValid, setCollegeNameIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  })
+  useEffect(() => {
+    console.log("Effect Running");
+    return () => {
+      console.log("Effect Cleanup");
+    };
+  }, []);
+
+  const {isValid: emailIsValid} = emailState;
+  const {isValid: passwordIsValid} = passwordState;
+
   useEffect(() =>{
-    setFormIsValid(
-      enteredCollegeName.trim().length > 0 && enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-  }, [enteredCollegeName, enteredEmail,enteredPassword])
+    const identifier = setTimeout(() =>{
+      console.log('CHecking for Validation')
+      setFormIsValid(
+        enteredCollegeName.trim().length > 0 && emailIsValid && passwordIsValid
+      );
+    }, 500);
+    return () =>{
+      console.log('CLEANUP')
+      clearTimeout(identifier)
+    }
+  }, [enteredCollegeName, emailIsValid, passwordIsValid])
 
   const collegeNameChangeHandler = (event) => {
-    setEnteredCollegeName(event.target.value)
-  }
+    setEnteredCollegeName(event.target.value);
+  };
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type: 'USER_INPUT', val: event.target.value})
+    // setFormIsValid(
+    //   enteredCollegeName.trim().length > 0 &&
+    //     event.target.value.includes("@") &&
+    //     passwordState.isValid
+    // );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({type: 'USER_INPUT', val: event.target.value})
+    // setFormIsValid(
+    //   enteredCollegeName.trim().length > 0 &&
+    //     emailState.isValid &&
+    //     event.target.value.trim().length > 6
+    // );
   };
 
-  const validateCollegeNameHandler = () =>{
-    setCollegeNameIsValid(enteredCollegeName.trim().length > 0)
-  }
+  const validateCollegeNameHandler = () => {
+    setCollegeNameIsValid(enteredCollegeName.trim().length > 0);
+  };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({type: 'INPUT_BLUR'})
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({type: 'INPUT_BLUR'})
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredCollegeName, enteredEmail, enteredPassword);
+    props.onLogin(enteredCollegeName, emailState.value, passwordState.value);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-      <div
+        <div
           className={`${classes.control} ${
-            collegeNameIsValid === false ? classes.invalid : ''
+            collegeNameIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="collegename">College Name</label>
@@ -67,28 +123,28 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
